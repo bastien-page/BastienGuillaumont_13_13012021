8be3800c9e8bd39fccfc96e38d9fda561763750e
Bastien <bab2a2sgt@gmail.com>
2022-01-13 15:17:03 +0100

    component Hero

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import Hero from "../components/_Hero";
 
 function Home() {
   return (
-    <div>
+    <div className="app">
       <Navbar />
+      <main className="main">
+        <Hero />
+      </main>
       <Footer />
     </div>
   );
